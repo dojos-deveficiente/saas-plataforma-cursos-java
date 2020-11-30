c47d16efac45769a833b642e6ad2df7b3ddd9af6
alberto souza <alots.ssa@gmail.com>
2020-11-30 20:05:16 -0300

    estrutura inicial

---
+++ src/main/java/com/deveficiente/saascursoonline/SaasCursoOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.deveficiente.saascursoonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaasCursoOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaasCursoOnlineApplication.class, args);
+	}
+
+}
