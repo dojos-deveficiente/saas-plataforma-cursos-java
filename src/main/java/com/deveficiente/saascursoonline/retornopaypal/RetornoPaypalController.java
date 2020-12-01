599b3c60fce7a46a8e6da9fbf0825920110c08f8
LeoSanGo <souoleo@gmail.com>
2020-11-30 22:39:58 -0300

    Implementação da Entity retornoPaypal

--- src/main/java/com/deveficiente/saascursoonline/retornopaypal/RetornoPaypalController.java
+++ src/main/java/com/deveficiente/saascursoonline/retornopaypal/RetornoPaypalController.java
@@ -1,5 +1,7 @@
 package com.deveficiente.saascursoonline.retornopaypal;
 
+import javax.validation.Valid;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class RetornoPaypalController {
 
     @PostMapping(path = "/retorno/paypal")
-    public ResponseEntity<?> execute(@RequestBody RetornoPaypalRequest request){
+    public ResponseEntity<?> execute(@RequestBody @Valid RetornoPaypalRequest request){
         return ResponseEntity.ok(null);
     }
 
--- src/main/java/com/deveficiente/saascursoonline/retornopaypal/RetornoPaypalRequest.java
+++ src/main/java/com/deveficiente/saascursoonline/retornopaypal/RetornoPaypalRequest.java
@@ -1,4 +1,35 @@
 package com.deveficiente.saascursoonline.retornopaypal;
 
+import java.math.BigDecimal;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 public class RetornoPaypalRequest {
+	
+	@Min(0)
+	@Max(1)
+	private int status;
+	@NotBlank
+	private String codigoTx;
+	@NotBlank
+	private String codigoApp;
+	@NotNull 
+	@Positive
+	private BigDecimal valor;
+	
+	public RetornoPaypalRequest(@Min(0) @Max(1) int status, @NotBlank String codigoTx, @NotBlank String codigoApp,
+			@NotNull @Positive BigDecimal valor) {
+		super();
+		this.status = status;
+		this.codigoTx = codigoTx;
+		this.codigoApp = codigoApp;
+		this.valor = valor;
+	}
+	
+	
+	
 }
